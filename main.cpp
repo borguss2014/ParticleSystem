4f6a2c0bf7310537357e897573add44562a8eab0
Cristian Spoiala <cristian.spoiala@bearingpoint.com>
2020-03-10 20:58:13 +0200

    Hooked imgui to clearcolor... just for fun

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 
 #include <stdio.h>
 
@@ -55,26 +55,8 @@ int main(int argc, char* argv[])
     // Setup Platform/Renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window.m_Window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'docs/FONTS.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = true;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
+    bool openWindow = true;
     
 //    particle_data data;
 //    data.position = glm::vec2(0.0f, 0.0f);
@@ -90,18 +72,20 @@ int main(int argc, char* argv[])
     particle_system particleSystem;
     particleSystem.Init();
     
-    /*particleSystem.SetAttribute(particle_attribute::POSITION, glm::vec2(0.0f, 0.0f));
-    particleSystem.SetAttribute(particle_attribute::VELOCITY, glm::vec2(1.0f, 1.0f));
-    particleSystem.SetAttribute(particle_attribute::COLOR_BEGIN, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
-    particleSystem.SetAttribute(particle_attribute::COLOR_END, glm::vec4(0.0f, 0.0f, 1.0f, 0.0f));
-    particleSystem.SetAttribute(particle_attribute::SCALE_BEGIN, glm::vec3(0.0f, 0.0f, 0.0f));
-    particleSystem.SetAttribute(particle_attribute::SCALE_END, glm::vec3(4.5f, 4.5f, 0.0f));
-    particleSystem.SetAttribute(particle_attribute::TOTAL_LIFE, 3);
-    particleSystem.SetAttribute(particle_attribute::EMISSION_RATE, 10);
-    particleSystem.SetAttribute(particle_attribute::EMISSION_FREQUENCY, 1.0f);*/
+//    particleSystem.SetAttribute(particle_attribute::POSITION, glm::vec2(0.0f, 0.0f));
+//    particleSystem.SetAttribute(particle_attribute::VELOCITY, glm::vec2(1.0f, 1.0f));
+//    particleSystem.SetAttribute(particle_attribute::COLOR_BEGIN, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+//    particleSystem.SetAttribute(particle_attribute::COLOR_END, glm::vec4(0.0f, 0.0f, 1.0f, 0.0f));
+//    particleSystem.SetAttribute(particle_attribute::SCALE_BEGIN, glm::vec3(0.0f, 0.0f, 0.0f));
+//    particleSystem.SetAttribute(particle_attribute::SCALE_END, glm::vec3(4.5f, 4.5f, 0.0f));
+//    particleSystem.SetAttribute(particle_attribute::TOTAL_LIFE, 3);
+//    particleSystem.SetAttribute(particle_attribute::EMISSION_RATE, 10);
+//    particleSystem.SetAttribute(particle_attribute::EMISSION_FREQUENCY, 1.0f);
     
     particleSystem.looping = true;
     particleSystem.Emit();
+    
+    glm::vec4 myColor = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
 
 	while (!glfwWindowShouldClose(window.m_Window))
 	{
@@ -110,10 +94,12 @@ int main(int argc, char* argv[])
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Begin("Another Window", &openWindow);
         ImGui::Text("Hello from another window!");
-        if (ImGui::Button("Close Me"))
-            show_another_window = false;
+        if (ImGui::Button("Close Me")) {
+            glfwSetWindowShouldClose(window.m_Window, true);
+        }
+        ImGui::ColorEdit4("Color", (float*)&myColor);
         ImGui::End();
 
         ImGui::Render();
@@ -129,7 +115,7 @@ int main(int argc, char* argv[])
 
         glfwSetWindowTitle(window.m_Window, ss.str().c_str());
         
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(myColor.r, myColor.g, myColor.b, myColor.a);
 		glClear(GL_COLOR_BUFFER_BIT);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
